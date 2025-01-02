ed62044f06e0d8515a1308b399aff17e53c967ca
Esmil Canet <148603535+esmilc@users.noreply.github.com>
2025-01-02 12:22:23 -0500

    Added New-Dice-Roller

---
+++ HTML-Journey/New-Dice-Roller/index.js
@@ -0,0 +1,19 @@
+
+function rollDice() {
+    const numofDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let _ = 0; _ < numofDice; _++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(` <img src="dice-images/${value}.png" alt="Dice ${value}">`)
+    }
+    console.log(images);
+
+    diceResult.textContent = `Dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join("");
+
+}
